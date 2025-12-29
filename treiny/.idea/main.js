ac41fed55dfc092de78641b0b240865172837385
VitaMirosh <miroshnichenko.vitaliya1992@gmail.com>
2025-12-29 11:30:30 +0300

    fix HW3 component

--- treiny/.idea/main.js
+++ treiny/.idea/main.js
@@ -26,13 +26,54 @@
 //
 //   listsEl.append(listEl)
 // })
-// rootEl.append(listsEl)
-function power(base, exponent){
- let mult = 1;
- for (let i = 0; i < exponent; i++){
-   mult *= base ;
- }
- return mult
+// // rootEl.append(listsEl)
+// function power(base, exponent){
+//  let mult = 1;
+//  for (let i = 0; i < exponent; i++){
+//    mult *= base ;
+//  }
+//  return mult
+// }
+//
+// console.log(power(4, -2))
+
+const todolistId_1 = crypto.randomUUID()
+const todolistId_2 = crypto.randomUUID()
+
+let todolist = [{
+  id: todolistId_1,
+  title: "What to lear",
+  filter: "all",
+  // tasks: [
+  //   {id: crypto.randomUUID(), title: "HTML", isDone: false},
+  //   {id: crypto.randomUUID(), title: "CSS", isDone: true},
+  //   {id: crypto.randomUUID(), title: "JS/TS", isDone: true},
+  // ]
+},
+  {
+    id: todolistId_2,
+    title: "What to buy",
+    filter: "all",
+    // tasks: [
+    //   {id: crypto.randomUUID(), title: "Brad", isDone: false},
+    //   {id: crypto.randomUUID(), title: "Meat", isDone: true},
+    //   {id: crypto.randomUUID(), title: "Milk", isDone: true},
+    // ]
+  }
+
+]
+const tasks = {
+  [todolistId_1]: [
+    {id: crypto.randomUUID(), title: "HTML", isDone: false},
+    {id: crypto.randomUUID(), title: "CSS", isDone: true},
+    {id: crypto.randomUUID(), title: "JS/TS", isDone: true},
+  ],
+  [todolistId_2]: [
+    {id: crypto.randomUUID(), title: "Brad", isDone: false},
+    {id: crypto.randomUUID(), title: "Meat", isDone: true},
+    {id: crypto.randomUUID(), title: "Milk", isDone: true},
+  ]
+
 }
 
-console.log(power(4, -2))
+
