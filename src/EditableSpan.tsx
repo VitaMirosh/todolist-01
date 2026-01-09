00f4adf43ef3ae4718ebdc924a10018caeb3bfc5
VitaMirosh <miroshnichenko.vitaliya1992@gmail.com>
2026-01-09 18:47:16 +0300

    create component EditableSpan

---
+++ src/EditableSpan.tsx
@@ -0,0 +1,35 @@
+import {ChangeEvent, useState} from 'react';
+
+type Props = {
+  value: string
+  onChange:(title:string)=>void
+}
+
+export const EditableSpan = ({value,onChange}: Props) => {
+  const [isEditMode, setIsEditMode] = useState(false)
+  const [title, setTitle] = useState(value)
+
+  const turnOnEditMode = () => {
+    setIsEditMode(true)
+    setTitle(title)
+  }
+
+  const changeTitle =(event:ChangeEvent<HTMLInputElement>)=>{
+    setTitle(event.currentTarget.value)
+  }
+  const turnOffEditMode = () => {
+    setIsEditMode(false)
+    onChange(title)
+  }
+
+  return (
+    <>
+      {isEditMode ? (
+        <input value={title} onChange={changeTitle} onBlur={turnOffEditMode} autoFocus/>
+      ) : (
+        <span onDoubleClick={turnOnEditMode}>{value}</span>
+      )}
+    </>
+  );
+};
+
