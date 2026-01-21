a2a97f787a15088917f4cdf5c178d9f153cc205e
VitaMirosh <miroshnichenko.vitaliya1992@gmail.com>
2026-01-21 21:20:15 +0300

    create uni-test and action creator to function delete_todolist

--- src/model/todolists-reducer.ts
+++ src/model/todolists-reducer.ts
@@ -6,13 +6,16 @@ const initialState: Todolist[] = []
 export const todolistsReducer = (state: Todolist[] = initialState, action: Actions): Todolist[] => {
   switch (action.type) {
     case 'delete_todolist': {
-      return state
+      return state.filter(todolist => todolist.id !== action.payload.id)
     }
     default:
       return state
   }
 }
-type Actions = {
-  type: string
-  payload: any
+export const deleteTodolistAC = (id:string)=>{
+  return {type:'delete_todolist', payload: { id }} as const
 }
+
+export type DeleteTodolistAction = ReturnType<typeof deleteTodolistAC>
+
+type Actions = DeleteTodolistAction
