e20ba4bb1cdefcf44eb87f11bc04c45e5a5a5e88
VitaMirosh <miroshnichenko.vitaliya1992@gmail.com>
2026-02-12 20:53:54 +0300

    create Task  component

--- src/TodolistItem.tsx
+++ src/TodolistItem.tsx
@@ -1,16 +1,12 @@
-import {ChangeEvent} from 'react';
 import {CreateItemForm} from './CreateItemForm.tsx';
-import {EditableSpan} from './EditableSpan.tsx';
-import {Box, Checkbox, IconButton, List, ListItem} from '@mui/material';
-import DeleteIcon from '@mui/icons-material/Delete';
+import {Box} from '@mui/material';
 import Button from '@mui/material/Button'
-import {containerSx, getListItemSx} from './TodolistItem.styles.ts';
+import {containerSx} from './TodolistItem.styles.ts';
 import {changeTodolistFilterAC, FilterValueTitle, Todolist} from '@/model/todolists-reducer.ts';
-import {useAppSelector} from '@/common/hooks/useAppSelector.ts';
-import {selectTasks} from '@/model/tasks-selector.ts';
-import {changeTaskStatusAC, changeTaskTitleAC, createTaskAC, deleteTaskAC} from '@/model/tasks-reducer.ts';
+import {createTaskAC} from '@/model/tasks-reducer.ts';
 import {useAppDispatch} from '@/common/hooks/useAppDispatch.ts';
 import {TodolistTitle} from '@/TodolistTitle.tsx';
+import {Tasks} from '@/Tasks.tsx';
 
 
 type Props = {
@@ -21,18 +17,10 @@ type Props = {
 export const TodolistItem = ({todolist}:Props)=>{
   const {id,filter}=todolist
 
-  const tasks = useAppSelector(selectTasks)
 
   const dispatch = useAppDispatch();
 
-  const todolistTasks = tasks[id]
-  let filteredTasks = todolistTasks
-  if (filter === 'active') {
-    filteredTasks = todolistTasks.filter(task => !task.isDone)
-  }
-  if (filter === 'completed') {
-    filteredTasks = todolistTasks.filter(task => task.isDone)
-  }
+
   const changeFilterHandler = (filter: FilterValueTitle) => {
     dispatch(changeTodolistFilterAC({id, filter}));
   }
@@ -41,38 +29,12 @@ export const TodolistItem = ({todolist}:Props)=>{
     dispatch(createTaskAC({id: id, title}))
   }
 
-
   return (
     <div>
       <TodolistTitle todolist={todolist}/>
       <CreateItemForm createItem={createItemHandler}/>
-      {filteredTasks.length === 0 ? (<p>"Тасок нет"</p>) :
-        <List>
-          {filteredTasks.map(task => {
-            const deleteTaskHandler = () => {
-              dispatch(deleteTaskAC({id: id, taskId: task.id}))
-            }
-            const changeTaskStatusHandler = (event: ChangeEvent<HTMLInputElement>) => {
-              dispatch(changeTaskStatusAC({id: id, taskID: task.id, isDone: event.currentTarget.checked}))
-            }
-            const changeTaskTitleHandler = (title: string) => {
-              dispatch(changeTaskTitleAC({id:id, taskID:task.id, title}))
-            }
+     <Tasks todolist={todolist}/>
 
-            return (
-              <ListItem key={task.id}
-                        sx={getListItemSx(task.isDone)}>
-                <div>
-                  <Checkbox checked={task.isDone} onChange={changeTaskStatusHandler}/>
-                  <EditableSpan value={task.title} onChange={changeTaskTitleHandler}/>
-                </div>
-                <IconButton onClick={deleteTaskHandler}>
-                  <DeleteIcon/>
-                </IconButton>
-              </ListItem>
-            )
-          })}
-        </List>}
 
       <Box sx={containerSx}>
         <Button variant={filter === 'all' ? 'outlined' : 'text'}
