97cc3ded9831b709bcde4e4855a50856eb35fe0c
VitaMirosh <miroshnichenko.vitaliya1992@gmail.com>
2026-03-08 20:13:37 +0300

    fix:Todolists

--- src/features/todolists/ui/Todolists/Todolists.tsx
+++ src/features/todolists/ui/Todolists/Todolists.tsx
@@ -1,17 +1,20 @@
 import { useAppDispatch, useAppSelector } from "@/common/hooks"
-import { TodolistItem } from "./TodolistItem/TodolistItem"
+import { fetchTodolistsTC, selectTodolists } from "@/features/todolists/model/todolists-slice"
+
 import Paper from "@mui/material/Paper"
-import { Grid } from "@mui/material"
 import { useEffect } from "react"
-import { fetchTodolistsTC, selectTodolists } from "@/features/todolists/model/todolists-slice.ts"
+import { TodolistItem } from "./TodolistItem/TodolistItem"
+import { Grid } from "@mui/material"
 
 export const Todolists = () => {
   const todolists = useAppSelector(selectTodolists)
+
   const dispatch = useAppDispatch()
 
   useEffect(() => {
     dispatch(fetchTodolistsTC())
-  }, [dispatch])
+  }, [])
+
   return (
     <>
       {todolists.map((todolist) => (
