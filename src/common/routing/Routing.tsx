593f4ee2b48194da71cbe0943f961047362280ae
VitaMirosh <miroshnichenko.vitaliya1992@gmail.com>
2026-03-20 12:40:24 +0300

    add:Routing

---
+++ src/common/routing/Routing.tsx
@@ -0,0 +1,17 @@
+import { Route, Routes } from "react-router"
+import { Main } from "@/app/Main.tsx"
+import { Login } from "@/features/todolists/ui/Login/Login"
+import { PageNotFound } from "@/common/components"
+export const Path = {
+  Main: "/",
+  Login: "login",
+  NotFound: "*",
+} as const
+
+export const Routing = () => (
+  <Routes>
+    <Route path={Path.Main} element={<Main />} />
+    <Route path={Path.Login} element={<Login />} />
+    <Route path={Path.NotFound} element={<PageNotFound />} />
+  </Routes>
+)
