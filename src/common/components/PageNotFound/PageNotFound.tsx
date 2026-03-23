e4c706f8e34e2e4616911a21ed85b2ffd673a3d3
VitaMirosh <miroshnichenko.vitaliya1992@gmail.com>
2026-03-23 21:19:30 +0300

    add:style,link to pageNotFound

--- src/common/components/PageNotFound/PageNotFound.tsx
+++ src/common/components/PageNotFound/PageNotFound.tsx
@@ -1,8 +1,12 @@
 import styles from "./PageNotFound.module.css"
+import { NavButton } from "@/common/components"
 
 export const PageNotFound = () => (
   <>
     <h1 className={styles.title}>404</h1>
     <h2 className={styles.subtitle}>page not found</h2>
+    <NavButton href={"/"} style={{ maxWidth: "310px", height: "45px", margin: "0 auto", marginTop: "30px" }}>
+      Вернуться на главную
+    </NavButton>
   </>
 )
