0ec86ddab5e46d3dae91243e1f07b5fac237e378
VitaMirosh <miroshnichenko.vitaliya1992@gmail.com>
2026-03-28 14:21:32 +0300

    add:styles circularProgressContainer;add:me request

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,20 +1,39 @@
-import "./App.css"
-import { selectThemeMode } from "@/app/app-slice"
+import styles from "./App.module.css"
 import { ErrorSnackbar, Header } from "@/common/components"
-import { useAppSelector } from "@/common/hooks"
+import { useAppDispatch, useAppSelector } from "@/common/hooks"
 import { Routing } from "@/common/routing"
 import { getTheme } from "@/common/theme"
 import CssBaseline from "@mui/material/CssBaseline"
 import { ThemeProvider } from "@mui/material/styles"
+import { useEffect, useState } from "react"
+import { meTC } from "@/features/auth/model/auth-slice.ts"
+import { CircularProgress } from "@mui/material"
+import { selectThemeMode } from "@/app/app-slice.ts"
 
 export const App = () => {
+  const [init, setIsInit] = useState(false)
   const themeMode = useAppSelector(selectThemeMode)
 
   const theme = getTheme(themeMode)
+  const dispatch = useAppDispatch()
+  useEffect(() => {
+    dispatch(meTC())
+      .unwrap()
+      .finally(() => {
+        setIsInit(false)
+      })
+  }, [])
 
+  if (!init) {
+    return (
+      <div className={styles.circularProgressContainer}>
+        <CircularProgress size={150} thickness={3} />
+      </div>
+    )
+  }
   return (
     <ThemeProvider theme={theme}>
-      <div className={"app"}>
+      <div className={styles.app}>
         <CssBaseline />
         <Header />
         <Routing />
