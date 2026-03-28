3a73fec28bf36faad123a4512d75ff5c4c9202c4
VitaMirosh <miroshnichenko.vitaliya1992@gmail.com>
2026-03-28 14:18:24 +0300

    DELETE:NavButton-Sign in and Sign up;create:Sign out

--- src/common/components/Header/Header.tsx
+++ src/common/components/Header/Header.tsx
@@ -10,10 +10,12 @@ import IconButton from "@mui/material/IconButton"
 import Switch from "@mui/material/Switch"
 import Toolbar from "@mui/material/Toolbar"
 import LinearProgress from "@mui/material/LinearProgress"
+import { logoutTC, selectIsLoggedIn } from "@/features/auth/model/auth-slice.ts"
 
 export const Header = () => {
   const themeMode = useAppSelector(selectThemeMode)
   const status = useAppSelector(selectAppStatus)
+  const isLoggedIn = useAppSelector(selectIsLoggedIn)
 
   const dispatch = useAppDispatch()
 
@@ -22,7 +24,9 @@ export const Header = () => {
   const changeMode = () => {
     dispatch(changeThemeModeAC({ themeMode: themeMode === "light" ? "dark" : "light" }))
   }
-
+  const logoutHandler = () => {
+    dispatch(logoutTC())
+  }
   return (
     <AppBar position="static" sx={{ mb: "30px" }}>
       <Toolbar>
@@ -31,8 +35,7 @@ export const Header = () => {
             <MenuIcon />
           </IconButton>
           <div>
-            <NavButton>Sign in</NavButton>
-            <NavButton>Sign up</NavButton>
+            {isLoggedIn && <NavButton onClick={logoutHandler}>Logout</NavButton>}
             <NavButton background={theme.palette.primary.dark}>Faq</NavButton>
             <Switch color={"default"} onChange={changeMode} />
           </div>
