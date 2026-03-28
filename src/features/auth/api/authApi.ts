269c62b1b4d86534b71842b85abba6a5807c8868
VitaMirosh <miroshnichenko.vitaliya1992@gmail.com>
2026-03-28 14:11:22 +0300

    create authApi,authApi

---
+++ src/features/auth/api/authApi.ts
@@ -0,0 +1,15 @@
+import { LoginInputs } from "@/features/auth/lib/schemas"
+import { instance } from "@/common/instance"
+import { BaseResponse } from "@/common/types"
+
+export const authApi = {
+  login(payload: LoginInputs) {
+    return instance.post<BaseResponse<{ userId: number; token: string }>>(`/auth/login`, payload)
+  },
+  logout() {
+    return instance.delete<BaseResponse>(`/auth/login`)
+  },
+  me() {
+    return instance.get<BaseResponse<{ id: string; email: string; login: number }>>(`/auth/me`)
+  },
+}
