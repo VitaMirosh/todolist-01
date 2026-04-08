6ae8433009abdde3755e07eb70209e42f0b03df3
VitaMirosh <miroshnichenko.vitaliya1992@gmail.com>
2026-04-08 14:37:21 +0300

    change dispatch and useSelector to hook useCreateTodolistMutation

--- src/app/Main.tsx
+++ src/app/Main.tsx
@@ -4,18 +4,20 @@ import { createTodolistTC } from "@/features/todolists/model/todolists-slice"
 import { Todolists } from "@/features/todolists/ui/Todolists/Todolists"
 import Container from "@mui/material/Container"
 import Grid from "@mui/material/Grid"
+import { useCreateTodolistMutation } from "@/features/todolists/api/todolistsApi.ts"
 
 export const Main = () => {
-  const dispatch = useAppDispatch()
-
-  const createTodolist = (title: string) => {
-    dispatch(createTodolistTC(title))
+  const [createTodolist] = useCreateTodolistMutation()
+  // const dispatch = useAppDispatch()
+  //
+  const createTodolistHandler = (title: string) => {
+    createTodolist(title)
   }
 
   return (
     <Container maxWidth={"lg"}>
       <Grid container sx={{ mb: "30px" }}>
-        <CreateItemForm onCreateItem={createTodolist} />
+        <CreateItemForm onCreateItem={createTodolistHandler} />
       </Grid>
       <Grid container spacing={4}>
         <Todolists />
