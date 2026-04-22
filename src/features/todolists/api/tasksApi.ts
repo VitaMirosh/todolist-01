1093a542a3e8dd210acf1905982d25195a6e5926
VitaMirosh <miroshnichenko.vitaliya1992@gmail.com>
2026-04-22 12:56:17 +0300

    add Query to taskApi:createTask,updateTask,deleteTask

--- src/features/todolists/api/tasksApi.ts
+++ src/features/todolists/api/tasksApi.ts
@@ -9,6 +9,35 @@ export const tasksApi = baseApi.injectEndpoints({
       query: (todolistId) => `/todo-lists/${todolistId}/tasks`,
       providesTags: ["Task"],
     }),
+    createTask: builder.mutation<BaseResponse<{ item: DomainTask }>, { todolistId: string; title: string }>({
+      query: ({ todolistId, title }) => ({
+        method: "post",
+        url: `/todo-lists/${todolistId}/tasks`,
+        body: { title },
+      }),
+      invalidatesTags: ["Task"],
+    }),
+    updateTask: builder.mutation<
+      BaseResponse<{ item: DomainTask }>,
+      {
+        todolistId: string
+        taskId: string
+        model: UpdateTaskModel
+      }
+    >({
+      query: ({ todolistId, taskId, model }) => ({
+        method: "put",
+        url: `/todo-lists/${todolistId}/tasks/${taskId}`,
+        body: model,
+      }),
+      invalidatesTags: ["Task"],
+    }),
+    deleteTask: builder.mutation<BaseResponse, { todolistId: string; taskId: string }>({
+      query: ({ todolistId, taskId }) => ({
+        method: "delete",
+        url: `/todo-lists/${todolistId}/tasks/${taskId}`,
+      }),
+    }),
   }),
 })
 
