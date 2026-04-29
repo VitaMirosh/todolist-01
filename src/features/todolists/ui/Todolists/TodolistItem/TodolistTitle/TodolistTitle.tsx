738dc8b2c1b2e3e8ae80ddc4436140b39fa7a559
VitaMirosh <miroshnichenko.vitaliya1992@gmail.com>
2026-04-29 14:52:24 +0300

    add: extraReducer with addMatcher

--- src/features/todolists/ui/Todolists/TodolistItem/TodolistTitle/TodolistTitle.tsx
+++ src/features/todolists/ui/Todolists/TodolistItem/TodolistTitle/TodolistTitle.tsx
@@ -1,9 +1,16 @@
 import { EditableSpan } from "@/common/components"
-import { useRemoveTodolistMutation, useUpdateTodolistTitleMutation } from "@/features/todolists/api/todolistsApi"
+import {
+  todolistsApi,
+  useRemoveTodolistMutation,
+  useUpdateTodolistTitleMutation,
+} from "@/features/todolists/api/todolistsApi"
 import { type DomainTodolist } from "@/features/todolists/model/todolists-slice"
 import DeleteIcon from "@mui/icons-material/Delete"
 import IconButton from "@mui/material/IconButton"
 import styles from "./TodolistTitle.module.css"
+import { useAppDispatch } from "@/common/hooks"
+import { RequestStatus } from "@/common/types"
+import { ResultCode } from "@/common/enums"
 
 type Props = {
   todolist: DomainTodolist
@@ -11,12 +18,33 @@ type Props = {
 
 export const TodolistTitle = ({ todolist }: Props) => {
   const { id, title, entityStatus } = todolist
-
+  const dispatch = useAppDispatch()
   const [removeTodolist] = useRemoveTodolistMutation()
   const [updateTodolistTitle] = useUpdateTodolistTitleMutation()
 
+  const changeEntityStatus = (entityStatus: RequestStatus) => {
+    dispatch(
+      todolistsApi.util.updateQueryData("getTodolists", undefined, (todolists) => {
+        const todolist = todolists.find((todolist) => todolist.id === id)
+        if (todolist) {
+          todolist.entityStatus = entityStatus
+        }
+      }),
+    )
+  }
+
   const deleteTodolist = () => {
+    changeEntityStatus("loading")
     removeTodolist(id)
+      .unwrap()
+      .then((data) => {
+        if (data.resultCode === ResultCode.Error) {
+          changeEntityStatus("failed")
+        }
+      })
+      .catch(() => {
+        changeEntityStatus("failed")
+      })
   }
 
   const changeTodolistTitle = (title: string) => {
