6790e6eba4fa28dfbaca8ffc59ab0b8ac27abd07
VitaMirosh <miroshnichenko.vitaliya1992@gmail.com>
2026-05-19 17:05:49 +0300

    feat: add captcha in endpoint,slice and login form

--- src/app/app-slice.ts
+++ src/app/app-slice.ts
@@ -8,12 +8,14 @@ export const appSlice = createSlice({
     status: "idle" as RequestStatus,
     error: null as string | null,
     isLoggedIn: false,
+    captcha: false,
   },
   selectors: {
     selectThemeMode: (state) => state.themeMode,
     selectAppStatus: (state) => state.status,
     selectAppError: (state) => state.error,
     selectIsLoggedIn: (state) => state.isLoggedIn,
+    selectIsCaptcha: (state) => state.captcha,
   },
   reducers: (create) => ({
     changeThemeModeAC: create.reducer<{ themeMode: ThemeMode }>((state, action) => {
@@ -27,6 +29,9 @@ export const appSlice = createSlice({
     }),
     setIsLoggedInAC: create.reducer<{ isLoggedIn: boolean }>((state, action) => {
       state.isLoggedIn = action.payload.isLoggedIn
+    }),
+    setIsCaptchaAC: create.reducer<{ captcha: boolean }>((state, action) => {
+      state.captcha = action.payload.captcha
     }),
   }),
   extraReducers: (builder) => {
@@ -44,7 +49,7 @@ export const appSlice = createSlice({
 })
 
 export const { selectThemeMode, selectAppStatus, selectAppError, selectIsLoggedIn } = appSlice.selectors
-export const { changeThemeModeAC, setAppStatusAC, setAppErrorAC, setIsLoggedInAC } = appSlice.actions
+export const { changeThemeModeAC, setAppStatusAC, setAppErrorAC, setIsLoggedInAC, setIsCaptchaAC } = appSlice.actions
 export const appReducer = appSlice.reducer
 
 export type ThemeMode = "dark" | "light"
--- src/features/auth/ui/Login/Login.tsx
+++ src/features/auth/ui/Login/Login.tsx
@@ -1,9 +1,9 @@
-import { selectThemeMode, setIsLoggedInAC } from "@/app/app-slice"
+import { selectThemeMode, setIsCaptchaAC, setIsLoggedInAC } from "@/app/app-slice"
 import { AUTH_TOKEN } from "@/common/constants"
 import { ResultCode } from "@/common/enums"
 import { useAppDispatch, useAppSelector } from "@/common/hooks"
 import { getTheme } from "@/common/theme"
-import { useLoginMutation } from "@/features/auth/api/authApi"
+import { useLazyCaptchaQuery, useLoginMutation } from "@/features/auth/api/authApi"
 import { type LoginInputs, loginSchema } from "@/features/auth/lib/schemas"
 import { zodResolver } from "@hookform/resolvers/zod"
 import Button from "@mui/material/Button"
@@ -19,13 +19,12 @@ import styles from "./Login.module.css"
 
 export const Login = () => {
   const themeMode = useAppSelector(selectThemeMode)
-
   const [login] = useLoginMutation()
+  const [captchaData, { data }] = useLazyCaptchaQuery()
 
   const dispatch = useAppDispatch()
 
   const theme = getTheme(themeMode)
-
   const {
     register,
     handleSubmit,
@@ -38,66 +37,98 @@ export const Login = () => {
   })
 
   const onSubmit: SubmitHandler<LoginInputs> = (data) => {
-    login(data).then((res) => {
+    login({ ...data }).then((res) => {
       if (res.data?.resultCode === ResultCode.Success) {
         dispatch(setIsLoggedInAC({ isLoggedIn: true }))
         localStorage.setItem(AUTH_TOKEN, res.data.data.token)
         reset()
       }
+      if (res.data?.resultCode === ResultCode.CaptchaError) {
+        captchaData()
+        dispatch(setIsCaptchaAC({ captcha: true }))
+        reset()
+      }
     })
   }
-
+  const images = data ? data.url : undefined
   return (
-    <Grid container justifyContent={"center"}>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <FormControl>
-          <FormLabel>
-            <p>
-              To login get registered
-              <a
-                style={{ color: theme.palette.primary.main, marginLeft: "5px" }}
-                href="https://social-network.samuraijs.com"
-                target="_blank"
-                rel="noreferrer"
-              >
-                here
-              </a>
-            </p>
-            <p>or use common test account credentials:</p>
-            <p>
-              <b>Email:</b> free@samuraijs.com
-            </p>
-            <p>
-              <b>Password:</b> free
-            </p>
-          </FormLabel>
-          <FormGroup>
-            <TextField label="Email" margin="normal" error={!!errors.email} {...register("email")} />
-            {errors.email && <span className={styles.errorMessage}>{errors.email.message}</span>}
-            <TextField
-              type="password"
-              label="Password"
-              margin="normal"
-              error={!!errors.email}
-              {...register("password")}
-            />
-            {errors.password && <span className={styles.errorMessage}>{errors.password.message}</span>}
-            <FormControlLabel
-              label={"Remember me"}
-              control={
-                <Controller
-                  name={"rememberMe"}
-                  control={control}
-                  render={({ field: { value, ...field } }) => <Checkbox {...field} checked={value} />}
-                />
-              }
-            />
-            <Button type="submit" variant="contained" color="primary">
-              Login
-            </Button>
-          </FormGroup>
-        </FormControl>
-      </form>
-    </Grid>
+    <>
+      <Grid container justifyContent={"center"}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <FormControl>
+            <FormLabel>
+              <p>
+                To login get registered
+                <a
+                  style={{ color: theme.palette.primary.main, marginLeft: "5px" }}
+                  href="https://social-network.samuraijs.com"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  here
+                </a>
+              </p>
+              <p>or use common test account credentials:</p>
+              <p>
+                <b>Email:</b> free@samuraijs.com
+              </p>
+              <p>
+                <b>Password:</b> free
+              </p>
+            </FormLabel>
+            <FormGroup>
+              <TextField label="Email" margin="normal" error={!!errors.email} {...register("email")} />
+              {errors.email && <span className={styles.errorMessage}>{errors.email.message}</span>}
+              <TextField
+                type="password"
+                label="Password"
+                margin="normal"
+                error={!!errors.email}
+                {...register("password")}
+              />
+              {errors.password && <span className={styles.errorMessage}>{errors.password.message}</span>}
+              <FormControlLabel
+                label={"Remember me"}
+                control={
+                  <Controller
+                    name={"rememberMe"}
+                    control={control}
+                    render={({ field: { value, ...field } }) => <Checkbox {...field} checked={value} />}
+                  />
+                }
+              />
+              {data && (
+                <div style={{ display: "flex", flexDirection: "row", marginBottom: "10px" }}>
+                  <div
+                    style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}
+                  >
+                    <img src={images} alt={"12"} width={300} height={100} />
+                    <button
+                      type={"button"}
+                      style={{ width: "150px", backgroundColor: "lawngreen" }}
+                      onClick={() => captchaData()}
+                    >
+                      captcha refresh
+                    </button>
+                  </div>
+                  <TextField
+                    id={"outlined-basic"}
+                    label="captcha"
+                    margin="normal"
+                    error={!!errors.captcha}
+                    {...register("captcha")}
+                  />
+                  {errors.captcha && <span className={styles.errorMessage}>{errors.captcha.message}</span>}
+                </div>
+              )}
+
+              <Button type="submit" variant="contained" color="primary">
+                Login
+              </Button>
+            </FormGroup>
+          </FormControl>
+        </form>
+      </Grid>
+    </>
   )
 }
