e7c57acbbdaf19ff27258e4ecd067d3a4f0cecdf
VitaMirosh <miroshnichenko.vitaliya1992@gmail.com>
2026-05-20 18:52:37 +0300

    add library drad and drop

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/todolist-01/",
+  base: "/todolist-01",
   resolve: {
     alias: {
       "@/": `${path.resolve(__dirname, "src")}/`,
